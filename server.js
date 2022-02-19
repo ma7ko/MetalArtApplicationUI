e64105d5d039b298c84371c8794fafd3859f7b57
Marko Panushkovski <marko.panushkovski@pelagus-it.com>
2022-02-19 22:37:18 +0100

    new

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/metalartapplication'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/metalartapplication/src/index.html'));});
+'/index.html'));});
 app.listen(process.env.PORT || 8080);
