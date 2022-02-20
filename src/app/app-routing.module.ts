682ce37f597400f7c791394b3d2a78327be241c5
Marko Panushkovski <marko.panushkovski@pelagus-it.com>
2022-02-20 02:40:59 +0100

    Changed initial page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './shared/error/error.component';
 
 const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
   { path: 'payment', loadChildren: () => import('./payment/payment.module').then(m => m.PaymentModule) },
   { path: '**', component: ErrorComponent }
